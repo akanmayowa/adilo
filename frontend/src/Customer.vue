30e9868c40bf26b8b6f10c12e47e64acef5b2d98
akanmayowa <akanmayowa@yahoo.com>
2022-07-10 23:45:53 +0100

    first commit

---
+++ frontend/src/Customer.vue
@@ -0,0 +1,188 @@
+<template>
+    <div>
+        <nav class="bg-white-800 border-b-2 border-gray-200 ...">
+            <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 pt-2">
+                <div class="relative flex h-16">
+
+                    <div class="flex flex-wrap flex-row ">
+                        <img class="h-8 w-1/2 mt-1"
+                            src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg">
+                        <h3 class="text-black-300c -ml-10 mt-2 h-8 text-md font-medium">Adilo</h3>
+                    </div>
+
+
+                    <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                        <div class="hidden sm:block sm:ml-6">
+                            <div class="flex space-x-4">
+                                <a href="#"
+                                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                                    aria-current="page">Team</a>
+                                <a href="#"
+                                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Team</a>
+                                <a href="#"
+                                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
+                                <a href="#"
+                                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calendar</a>
+                                <a href="#"
+                                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Team</a>
+                                <a href="#"
+                                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calendar</a>
+
+                                <div class="flex">
+                                    <span class="mt-2 ml-20 pr-2">help</span>
+                                    <div class=" pl-5 mt-2 border-l-2 border-gray-100 ...">
+                                        <div class="flex items-center space-x-4">
+                                            <div class="flex-shrink-0">
+                                                <img class="h-6 w-6 rounded-md" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80">
+                                            </div>
+                                            <div class="flex-1">
+                                                <p class="text-sm text-xs text-gray-900 dark:text-white"> Neil Sims </p>
+                                                <p class="text-sm text-xs text-gray-500 dark:text-gray-400"> email@windster.com </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div>
+        </nav>
+
+
+        <div class="flex flex-row">
+            <div class="basis-1/4 h-screen bg-slate-50">
+                <aside class="w-64" aria-label="Sidebar">
+                    <div class="overflow-y-auto py-4 px-3 bg-slate-50 rounded dark:bg-gray-800">
+                        <ul class="space-y-5 pl-10">
+                            <li>
+                                <a href="#"
+                                    class="flex items-center pl-2  text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700">
+                                    <svg class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                                        fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
+                                        <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
+                                    </svg>
+                                    <span class="ml-3">My Recordings</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                    class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700">
+                                    <svg class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                                        fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z">
+                                        </path>
+                                    </svg>
+                                    <span class="flex-1 ml-3 whitespace-nowrap">Requested</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </aside>
+            </div>
+
+
+
+
+
+            <div class="w-full">
+                <div class="relative left-20 mt-1 mx-auto">
+                    <div class="overflow-x-auto">
+                        <div class="">
+                            <div class="grid grid-cols-4 gap-4">
+                                <div class="py-3 px-5  text-sm ">
+                                    <ul class="flex">
+                                        <li><label href="#" class="font-semibold">SnapByte</label></li>
+                                        <li><label class="mx-2"> > </label></li>
+                                        <li><label href="#" class="font-semibold">My Recordings</label></li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="px-5 flex gap-4">
+                                <div class="mr-24">
+                                    <h3>My Recordings<span class="ml-2">25</span></h3>
+                                </div>
+<div class=""><button type="button" class="inline-block  px-8 py-1.5 border-2 border-gray-200 text-gray-400 font-medium text-xs leading-tight uppercase rounded-full">By Date</button> </div>
+<div class=""> <button type="button" class="inline-block   px-8 py-1.5 border-2 border-gray-200 text-gray-400 font-medium text-xs leading-tight uppercase rounded-full">Add Filter</button></div>
+<div class=""> <button type="button" class="inline-block  px-14 py-1.5 bg-blue-400 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md">New Request</button></div>
+<div class=""> <button type="button" @click="showModal = true"  class="inline-block  px-12 py-1.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md">Start Recording</button> </div>
+ <Modal :show="showModal"  @close="showModal=false"> </Modal> ,
+          
+          </div>
+                        </div>
+                        <table class="table-auto w-10/12 text-sm text-left text-gray-500 dark:text-gray-400 mt-10">
+                            <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="p-4">recordings </th>
+                                    <th scope="col" class="px-6 py-3"> Title</th>
+                                    <th scope="col" class="px-6 py-3"> views </th>
+                                    <th scope="col" class="px-4 py-3">Size</th>
+                                    <th scope="col" class="px-4 py-3">Last Modified </th>
+                                    <th scope="col" class="px-1 py-3">Edit</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="bg-white"  v-for="customer in customers.data" :key="customer.id">
+                                    <td class="w-4 p-4">
+                                        <div class="flex items-center">
+                                             <video width="250" height="150"  controls>
+                                            <source src="" type="video/mp4" >
+                                            </video>
+                                        </div>
+                                    </td>
+                                            
+                                    <td scope="row" class="px-6 py-4 text-dark-900 ">{{ customer.title}}  </td>
+                                    <td class="px-4 py-4"> {{ customer.views }}</td>
+                                    <td class="px-4 py-4"> {{ customer.size}}</td>
+                                    <td class="px-4 py-4"> {{ customer.last_modified }}</td>
+                                    <td class="px-1 py-4">...</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+     import Modal from "./Modal.vue"
+    import axios from "axios";
+    export default {
+        components: {
+             Modal
+        },
+
+
+        data() {
+            return {
+                 showModal: false,
+                customers: [],
+            }
+        },
+
+        created(){
+           const url = 'http://localhost:8000/api/fetch-customer-data/'
+            axios.get(url).then( response => {
+                this.customers = response.data;
+                console.log(this.customers);
+            });
+        },
+        
+    }
+
+
+
+
+</script>
